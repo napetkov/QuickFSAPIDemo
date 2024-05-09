baf96fdde73af880feb2769675903ed05af99f0f
napetkov <n.a.petkov@gmail.com>
2024-05-09 14:14:19 +0300

    Initialize Git Repository for Quickfs API Demo connection

---
+++ src/main/java/bg/softuni/quickfsapidemo/QuickfsApiDemoApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.quickfsapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickfsApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickfsApiDemoApplication.class, args);
+	}
+
+}
